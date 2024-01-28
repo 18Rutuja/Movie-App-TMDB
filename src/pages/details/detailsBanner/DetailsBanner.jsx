2a68455efcf9369775f5e786e36fc7665d5e4fbd
Rutuja Rathod <2021bec070@sggs.ac.in>
2024-01-28 15:21:11 +0530

    erroe solve

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -6,7 +6,7 @@ import dayjs from "dayjs";
 import "./DatailsBanner.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import useFetch from "../../../hooks/useFetch";
+import useFetch from "../../../Hooks/UseFetch";
 import Genres from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img.jsx";
