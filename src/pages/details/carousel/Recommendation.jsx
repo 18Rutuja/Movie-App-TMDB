40554d7447a1daaf2be68b20d3a132818ab56305
Rutuja Rathod <2021bec070@sggs.ac.in>
2024-01-28 15:31:26 +0530

    erroe solve

--- src/pages/details/carousel/Recommendation.jsx
+++ src/pages/details/carousel/Recommendation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from "../../../components/carousel/Carousel";
-import useFetch from "../../../hooks/useFetch";
+import useFetch from "../../../Hooks/UseFetch";
 
 const Recommendation = ({ mediaType, id }) => {
     const { data, loading, error } = useFetch(
